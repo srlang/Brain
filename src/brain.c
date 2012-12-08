ff96f212f214527daab7dcac532407d8fb172cf6
srlang <srlang@ncsu.edu>
2012-12-08 14:49:11 -0500

    Initial commit.

---
+++ src/brain.c
@@ -0,0 +1,13 @@
+/*
+ * brain.c - Part of the Brain program 
+ *      (a Brainf*** interpreter)
+ * Copyright (C) 2012 - Sean R. Lang
+ */
+
+
+#define SUCCESS_EXIT 0
+#define FAILURE_EXIT 1
+int main(int argc, int *argv[]) {
+    
+    return SUCCESS_EXIT;
+}
---
+++ src/parser.c
@@ -0,0 +1,7 @@
+/*
+ * parser.c - part of the Brain program
+ *      (a simple brainf*** interpreter)
+ * Copyright (C) 2012 - Sean R. Lang
+ */
+
+
