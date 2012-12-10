a256230c33bb0fcbec0ec609d20a6b7c2afbbcc1
srlang <srlang@ncsu.edu>
2012-12-10 16:48:47 -0500

    Added a little implementation.
    Plan to rewrite so that it writes a C file, compiles it and then runs
    it.

--- src/brain.c
+++ src/brain.c
@@ -4,10 +4,35 @@
  * Copyright (C) 2012 - Sean R. Lang
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "parser.c"
 
-#define SUCCESS_EXIT 0
-#define FAILURE_EXIT 1
-int main(int argc, int *argv[]) {
-    
+
+//#define TAPE_SIZE 30000
+
+/* Array of cells. */
+//extern char tape[TAPE_SIZE];
+
+/* Pointer to current cell location in array. */
+//extern char * ptr = tape;
+
+#define SUCCESS_EXIT    0
+#define FAILURE_EXIT    1
+#define READ_MODE       "r"
+#define MAX_FILENAME_SIZE   500
+//#define NEW(t,n) (t) malloc(sizeof(
+int main(int argc, char * argv[]) {
+    ptr = 0;
+    FILE * f;
+    if (argc > 1) {
+        char * filename = (char *) malloc( MAX_FILENAME_SIZE * sizeof(char));
+        filename = strncpy(filename, argv[1], MAX_FILENAME_SIZE);
+        f = fopen(filename, READ_MODE);
+    } else {
+        f = stdin;
+    }
+    parse(f);
     return SUCCESS_EXIT;
 }
--- src/parser.c
+++ src/parser.c
@@ -4,4 +4,70 @@
  * Copyright (C) 2012 - Sean R. Lang
  */
 
+#include <stdio.h>
 
+
+
+#define INCREMENT_PTR   '>'
+#define DECREMENT_PTR   '<'
+#define INCREMENT_BYTE  '+'
+#define DECREMENT_BYTE  '-'
+#define OUTPUT_BYTE     '.'
+#define ACCEPT_BYTE     ','
+#define LOOP_BEGIN      '['
+#define LOOP_END        ']'
+int is_command(char c) {
+    return (c == INCREMENT_PTR) 
+        || (c == DECREMENT_PTR)
+        || (c == INCREMENT_BYTE)
+        || (c == DECREMENT_BYTE)
+        || (c == OUTPUT_BYTE)
+        || (c == ACCEPT_BYTE)
+        || (c == LOOP_BEGIN)
+        || (c == LOOP_END);
+}
+
+#define NUM_COMMANDS    8
+char COMMANDS[] = {'>', '<', '+', '-', '.', ',', '[', ']'};
+
+int which_command(char c) {
+    if (!is_command(c)) {
+        return 0;
+    }
+    for(int i = 0; i < NUM_COMMANDS; i++) {
+        if (c == COMMANDS[i]) {
+            return i+1;
+        }
+    }
+}
+
+void execute_command(char c) {
+
+}
+
+
+
+
+
+#define TAPE_SIZE   30000
+
+/* Array of cells. */
+extern char tape[TAPE_SIZE];
+
+/* Pointer to current cell location in array. */
+extern char * ptr;
+
+
+void parse(FILE * f) {
+    char c;
+    ptr = tape;
+    if (f == NULL) {
+        /* print error message */
+        return;
+    }
+    while ((c = fgetc(f)) != EOF) {
+        if (is_command(c)) {
+            /* write command to file */
+        }
+    }
+}
